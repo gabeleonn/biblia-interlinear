0db5a7a881587639709d43ea03a7d3a13a0154f1
Gabriel Leon <gableonn@gmail.com>
2021-05-04 18:01:28 -0300

    style: global styles setup

--- styles/global.ts
+++ styles/global.ts
@@ -1,7 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  body {
+  * {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body, input, button {
     font-family: sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    color: inherit;
   }
 `;
